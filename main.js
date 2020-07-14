13ec5a683c2b28c1255583aed173679e63b31353
fournotfourerror <fournotfourerror@gmail.com>
2020-07-14 16:08:33 +0530

    modified main.js

--- main.js
+++ main.js
@@ -25,11 +25,13 @@ var profile=(basic_info)=>{
 
 	var email=document.createElement("a");
 	email.href="mailto:"+basic_info.email;
+	email.classList.add("link");
 	email.textContent=basic_info.email;
 	child_one.append(email);
 
 	var mobile=document.createElement("a");
 	mobile.href="tel:"+basic_info.mobile;
+	mobile.classList.add("link");
 	mobile.textContent=basic_info.mobile;
 	child_one.append(mobile);
 
@@ -40,4 +42,37 @@ var profile=(basic_info)=>{
 
 var details=(details_info)=>{
 	// console.log(details_info);
+	var summaryHeading=document.createElement("h2");
+	summaryHeading.textContent="Summary";
+	var hr1=document.createElement("hr");
+	child_two.append(summaryHeading);
+	child_two.append(hr1);
+
+	var list=document.createElement("ul");
+	details_info.summary.map(i=>{
+		var listitem=document.createElement("li");
+		listitem.textContent=i;
+		list.append(listitem);
+	})
+
+	child_two.append(list);
+
+	var skillHeading=document.createElement("h2");
+	skillHeading.textContent="Skills";
+	child_two.append(skillHeading);
+	var skill_hr1=document.createElement("hr");
+	child_two.append(skill_hr1);
+
+	details_info.skills.map(j=>{
+		var skillType=document.createElement("h3");
+		skillType.textContent=j.type;
+		child_two.append(skillType);
+
+		j.values.map(j_value=>{
+			var skillValue=document.createElement("span");
+			skillValue.textContent=j_value;
+			skillValue.classList.add("skill");
+			child_two.append(skillValue);
+		})
+	})
 }
